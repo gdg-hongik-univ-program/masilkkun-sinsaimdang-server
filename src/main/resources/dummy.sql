b8e5ca358efa6a7e019d1618515ce110b66ec501
KAITOKIDDA <twk031206@naver.com>
2025-08-15 21:12:27 +0900

    chore: 더미 데이터 주소 수정

--- src/main/resources/dummy.sql
+++ src/main/resources/dummy.sql
@@ -32,8 +32,8 @@ INSERT INTO article_photos (article_id, photo_url, photo_order) VALUES
 
 INSERT INTO article_places (article_id, place_order, place_name, address, description)
 VALUES (1, 1, '신라갈비', '경기도 수원시 영통구 동수원로 538', '수원 3대 갈비 맛집'),
-       (1, 2, '누크녹', '경기 수원시 팔달구 화서문로31번길 14-32 1 2층', '누크녹은 골목에 위치해 있지만, 멀리서도 존재감 퐁퐁'),
-       (1, 3, '만석공원', '경기 수원시 장안구 송죽동 248', '밥 먹고 느즈막히 산책하기');
+       (1, 2, '누크녹', '경기도 수원시 팔달구 화서문로31번길 14-32 1 2층', '누크녹은 골목에 위치해 있지만, 멀리서도 존재감 퐁퐁'),
+       (1, 3, '만석공원', '경기도 수원시 장안구 송죽동 248', '밥 먹고 느즈막히 산책하기');
 
 
 -- =================================================================================================
@@ -50,8 +50,8 @@ INSERT INTO article_photos (article_id, photo_url, photo_order) VALUES
                                                                     (2, 'https://cdn.kbthink.com/content/dam/tam-dcp-cms/kbcontent/%EC%9E%90%EC%82%B0%EA%B4%80%EB%A6%AC/%EB%94%94%EC%BD%98-%EC%82%AC%EC%97%85%EC%9E%90-%EC%BD%98%ED%85%90%EC%B8%A0/%EC%83%81%EA%B6%8C%EB%B6%84%EC%84%9D/jeonpodong/Jeonpodong-MO-03-1.jpg', 2);
 
 INSERT INTO article_places (article_id, place_order, place_name, address, description)
-VALUES (2, 1, '해운대 블루라인파크', '부산시 해운대구 중동 948-1', '해변을 따라 달리는 스카이캡슐과 해변열차가 있습니다.'),
-       (2, 2, '송정해수욕장', '부산시 해운대구 송정동', '서핑으로 유명한 비교적 한적한 해변입니다.');
+VALUES (2, 1, '해운대 블루라인파크', '부산광역시 해운대구 중동 948-1', '해변을 따라 달리는 스카이캡슐과 해변열차가 있습니다.'),
+       (2, 2, '송정해수욕장', '부산광역시 해운대구 송정동', '서핑으로 유명한 비교적 한적한 해변입니다.');
 
 
 -- =================================================================================================
@@ -67,8 +67,8 @@ INSERT INTO article_photos (article_id, photo_url, photo_order) VALUES
                                                                     (3, 'https://eiec.kdi.re.kr/userdata/nara/202108/edit/aaauOBDTNDE3Kaf343HRx_1627448711563.jpg', 1);
 
 INSERT INTO article_places (article_id, place_order, place_name, address, description)
-VALUES (3, 1, '주남저수지', '경남 창원시 의창구 대산면 유등리', '철새들이 많아 아름다운 곳'),
-       (3, 2, '저도', '경남 창원시 의창구 대산면 유등리', '한적하지만 소박하고 아름다운 곳');
+VALUES (3, 1, '주남저수지', '경상남도 창원시 의창구 대산면 유등리', '철새들이 많아 아름다운 곳'),
+       (3, 2, '저도', '경상남도 창원시 의창구 대산면 유등리', '한적하지만 소박하고 아름다운 곳');
 
 
 -- =================================================================================================
@@ -84,5 +84,5 @@ INSERT INTO article_photos (article_id, photo_url, photo_order) VALUES
                                                                     (4, 'https://i.ytimg.com/vi/AdnXgPYZgR8/maxresdefault.jpg', 1);
 
 INSERT INTO article_places (article_id, place_order, place_name, address, description)
-VALUES (4, 1, '제주 맛집', '제주시 어딘가', '제주 전통의 맛'),
-       (4, 2, '제주 맛집', '제주시 어딘가', '제주 특산물의 맛');
+VALUES (4, 1, '제주 맛집', '제주특별자치도 제주시 어딘가', '제주 전통의 맛'),
+       (4, 2, '제주 맛집', '제주특별자치도 제주시 어딘가', '제주 특산물의 맛');
